745611c13f645e87838b305b4f2020c8bde8ab02
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2020-01-22 16:27:46 +0200

    Lazy load was added.

---
+++ public/js/lazy.js
@@ -0,0 +1,12 @@
+$(window).load(function () {
+	const team = $('.team-member');
+	team.eq(0).prepend('<img src="/img/team/alexceo.jpg" alt="">');
+	team.eq(1).prepend('<img src="/img/team/lilya.jpg" alt="">');
+	team.eq(2).prepend('<img src="/img/team/ilya.jpg" alt="">');
+	team.eq(3).prepend('<img src="/img/team/vikalena.jpg" alt="">');
+	team.eq(4).prepend('<img src="/img/team/kirill.jpg" alt="">');
+	team.eq(5).prepend('<img src="/img/team/pavel.jpg" alt="">');
+	team.eq(6).prepend('<img src="/img/team/nikita.jpg" alt="">');
+	team.eq(7).prepend('<img src="/img/team/vilen.jpg" alt="">');
+	team.eq(8).prepend('<img src="/img/team/polina.jpg" alt="">');
+});
