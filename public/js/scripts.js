9e3f3562a0a47568d4957065e5f32edba73cdcba
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-07-13 12:50:25 +0300

    Header was returned.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -167,45 +167,45 @@
 
   // ANIMATED MENU
 
-  // var cbpAnimatedHeader = (function() {
+  var cbpAnimatedHeader = (function() {
 
-  //   var docElem = document.documentElement,
-  //     header = document.querySelector('.navbar-default'),
-  //     didScroll = false,
-  //     changeHeaderOn = 50;
+    var docElem = document.documentElement,
+      header = document.querySelector('.navbar-default'),
+      didScroll = false,
+      changeHeaderOn = 50;
 
-  //   function init() {
-  //     window.addEventListener('scroll', function(event) {
-  //       if (!didScroll) {
-  //         didScroll = true;
-  //         setTimeout(scrollPage, 100);
-  //       }
-  //     }, false);
-  //     window.addEventListener('load', function(event) {
-  //       if (!didScroll) {
-  //         didScroll = true;
-  //         setTimeout(scrollPage, 100);
-  //       }
-  //     }, false);
-  //   }
+    function init() {
+      window.addEventListener('scroll', function(event) {
+        if (!didScroll) {
+          didScroll = true;
+          setTimeout(scrollPage, 100);
+        }
+      }, false);
+      window.addEventListener('load', function(event) {
+        if (!didScroll) {
+          didScroll = true;
+          setTimeout(scrollPage, 100);
+        }
+      }, false);
+    }
 
-  //   function scrollPage() {
-  //     var sy = scrollY();
-  //     if (sy >= changeHeaderOn) {
-  //       classie.add(header, 'navbar-shrink');
-  //     } else {
-  //       classie.remove(header, 'navbar-shrink');
-  //     }
-  //     didScroll = false;
-  //   }
+    function scrollPage() {
+      var sy = scrollY();
+      if (sy >= changeHeaderOn) {
+        classie.add(header, 'navbar-shrink');
+      } else {
+        classie.remove(header, 'navbar-shrink');
+      }
+      didScroll = false;
+    }
 
-  //   function scrollY() {
-  //     return window.pageYOffset || docElem.scrollTop;
-  //   }
+    function scrollY() {
+      return window.pageYOffset || docElem.scrollTop;
+    }
 
-  //   init();
+    init();
 
-  // })();
+  })();
   
 })(jQuery);
 
