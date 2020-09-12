4172834d3f3d33a86446f45abbd84628e9bef7f7
Vilen Kupriienko <pride2808@gmail.com>
2020-09-12 15:58:52 +0300

    Now page doesn't refresh after sending lead

---
+++ public/js/leads.js
@@ -0,0 +1,15 @@
+$('#popup-contact').submit(function (e) {
+	e.preventDefault();
+
+	const form = $(this);
+	const url = form.attr('action');
+
+	$.ajax({
+		type: 'POST',
+		url: url,
+		data: form.serialize(),
+		success: function (data) {
+			openThanksModal();
+		}
+	});
+});
--- public/js/popup.js
+++ public/js/popup.js
@@ -9,3 +9,14 @@ function closeForm() {
 	$('#popup-wrapper').css('opacity', 0);
 	$('#popup-wrapper').css('visibility', 'hidden');
 }
+
+function openThanksModal() {
+	//$('#popup-wrapper').css('display', 'flex');
+	$('#thanks-popup-wrapper').css('visibility', 'visible');
+	$('#thanks-popup-wrapper').css('opacity', 1);
+}
+
+function closeThanksModal() {
+	$('#thanks-popup-wrapper').css('opacity', 0);
+	$('#thanks-popup-wrapper').css('visibility', 'hidden');
+}
