8f0a87433b7460d19703ba57a8cbb71216adf85d
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-09-27 17:46:34 +0300

    Config was added.

---
+++ src/bot/config.js
@@ -0,0 +1,17 @@
+const BOT_DEV_TOKEN = process.env.BOT_DEV_TOKEN;
+const BOT_DEV_CHANNEL_ID = process.env.BOT_DEV_CHANNEL_ID;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_CHANNEL_ID = process.env.BOT_CHANNEL_ID;
+
+const config = {
+	dev: {
+		token: BOT_DEV_TOKEN,
+		channelId: BOT_DEV_CHANNEL_ID
+	},
+	prod: {
+		token: BOT_TOKEN,
+		channelId: BOT_CHANNEL_ID
+	}
+};
+
+module.exports = isProd ? config.prod : config.dev;
