da152d1364fa3b5fccaebd7a65cd4eb120e904d1
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-10-04 10:34:45 +0300

    Little changes in code.

--- src/bot/config.js
+++ src/bot/config.js
@@ -2,6 +2,7 @@ const BOT_DEV_TOKEN = process.env.BOT_DEV_TOKEN;
 const BOT_DEV_CHANNEL_ID = process.env.BOT_DEV_CHANNEL_ID;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const BOT_CHANNEL_ID = process.env.BOT_CHANNEL_ID;
+const NODE_ENV = process.env.NODE_ENV;
 
 const config = {
 	dev: {
